2000336fff2bd58025ffeafd77537fd6f07a8359
adamWinebarger <adam.k.winebarger@gmail.com>
2023-06-06 20:10:24 -0400

    Rebuilding the project with UIStoryboard since SwiftUI seems like it might not be capable here

---
+++ Question Maker Pro/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Question Maker Pro
+//
+//  Created by adam Winebarger on 6/6/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
